c11afcad817699f99db06afe22858afab700d637
Luka Trkla <lukatrkla@outlook.com>
2020-11-17 21:06:50 -0500

    Adding data tags
    Adding information pertaining to data loaded into the package

--- R/data.R
+++ R/data.R
@@ -6,25 +6,118 @@
 #' @source \url{http://www.biostatistics.online/dbPepNeo/index.php}
 #'
 #' @format A data frame with 179 rows and 13 variables:
-#' \describe{
-#'   \item{%rank}{The % rank of binding affinity between the HLA allele and the the mutated peptide.}
-#'   \item{bind level}{The binding level between the mutant peptide and its associated HLA Allele.}
-#'   \item{cancer}{Neoantigen's associated form of cancer.}
-#'   \item{confidence}{The confidence level of the neoantigen.}
-#'   \item{gene}{The name of the gene.}
-#'   \item{hla allele}{The HLA allele for the neoantigen.}
-#'   \item{mut affinity(nM)}{The binding affinity between the HLA allele and the mutated peptide.}
-#'   \item{mut peptide}{The mutated sequence of peptides for the neoantigen.}
-#'   \item{mutation}{The name of the mutation.}
-#'   \item{peptide length}{The name of the mutation in bp}
-#'   \item{reference}{Pubmed article link for source of neoantigen.}
-#'   \item{verification}{The form of verifying the mutation and HLA allele
-#'   interaction.}
-#'   \item{wt peptide}{Wild type peptide sequence for the mutation.}
-#'   ...
-#'  }
-#'  @examples
-#'   \dontrun{
-#'    lungData
-#'   }
+#'
 "lungData"
+#' RANBP9 Mutated Peptides
+#'
+#' A string of nucleotides for RANBP9 that has been modified to include
+#' neoantigen relevant mutations.
+#'
+#' @source \url{https://www.uniprot.org/uniprot/Q96S59}
+#'
+#' @format A string
+#'
+#'  @examples
+"RANBP9mutated"
+#' Produced graph_affinity_solution
+#'
+#' The solution to running graph_affinity_solution, graph_affinity("TP53", tp53mutated3times, 1)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"graph_affinity_solution"
+#' Produced mutation_gene_data_solution
+#'
+#' The solution to running mutation_gene_data("TP53", tp53mutated3times)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"mutation_gene_data_solution"
+#' Produced mutation_positions_solution
+#'
+#' The solution to mutation_positions("TP53", tp53mutated3times)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"mutation_positions_solution"
+#' Produced patient_affinity_visualizer_solution
+#'
+#' The solution to patient_affinity_visualizer(testingGeneSet, colNum=1, colorFlag=0)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"patient_affinity_visualizer_solution"
+#' Produced patient_location_visualizer_solution
+#'
+#' The solution to patient_location_visualizer(testingGeneSet, colNum=1, confidence=0)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"patient_location_visualizer_solution"
+#' Produced plot_mutation_location_solution
+#'
+#' The solution to plot_mutation_location("TP53", tp53mutated3times, confidence = 1)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"plot_mutation_location_solution"
+#' Produced solution_patient_neoantigen_list
+#'
+#' The solution to patient_neoantigen_list(testingGeneSet)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"solution_patient_neoantigen_list"
+#' Produced surrounding_peptides_solution'
+#'
+#' The solution to surrounding_peptides("TP53", tp53mutated3times, 3)
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"surrounding_peptides_solution"
+#' tp53mutated3times Mutated Peptides
+#'
+#' A string of nucleotides for tp53mutated3times that has been modified to include
+#' neoantigen relevant mutations in at least 3 locations.
+#'
+#' @source \url{https://www.uniprot.org/uniprot/E7EQX7#sequences}
+#'
+#' @format A string
+#'
+#'  @examples
+"tp53mutated3times"
+
+#' Produced testingGeneSet'
+#'
+#' An assortment of Genes to be used for testing/
+#'
+#' @source NULL
+#'
+#' @format NULL
+#'
+#'  @examples
+"testingGeneSet"
